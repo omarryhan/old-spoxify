0924fea4c21300abc7ab9bb37e8f143465914fa8
Omar Ryhan <omarryhan@gmail.com>
2021-03-22 21:49:43 +0200

    .

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (<>
+      <Head>
+        <title>
+            Spoxify (Moved)
+        </title>
+    </Head>
+                <p>
+            Spoxify has been moved to:&nbsp;
+            <a href="https://spotilities.netlify.app">
+                https://spotilities.netlify.app
+            </a>
+        </p>
+
+        <p>
+            Apologies for the inconvenience :)
+        </p>
+        </>
+  )
+}
